40c4b4c8600a94104d1de404eb5100379c02a057
Violet Lesunyane <mlesunyane@gmail.com>
2026-09-14 15:22:43 -0700

    Update create_tables.sql

--- Creating DB and Tables/create_tables.sql
+++ Creating DB and Tables/create_tables.sql
@@ -12,3 +12,26 @@ signup_date date
 
 --Checking the table was created properly
 select* from de_project1.public.customers
+
+-- 2.2. Products
+create table de_project1.public.products(
+product_id varchar (255), 
+product_name varchar (255),
+category varchar (255),
+unit_price decimal
+)
+
+--Checking the table was created properly
+select* from de_project1.public.products
+
+-- 2.3. Orders
+create table de_project1.public.orders(
+order_id varchar (255), 
+customer_id varchar (255), 
+product_id varchar (255), 
+order_date date, 
+quantity int
+)
+
+--Checking the table was created properly
+select* from de_project1.public.orders
